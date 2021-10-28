85e315976d303e91ab23e137aeb6432d881ad4de
Pedro Dimas <p.dimas@campus.fct.unl.pt>
2021-10-28 13:59:05 +0100

    AutoGenerated Code is here Baby

---
+++ src/test/java/pt/unl/fct/SCC/SccApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.unl.fct.SCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
